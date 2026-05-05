ba88df8ba8536b59daa4e71ec3a1e3309e83f4af
woonjeng <woonjeng@gmail.com>
2026-05-05 01:57:59 +0000

    adding price level tests

--- tests/test_price_level.cpp
+++ tests/test_price_level.cpp
@@ -1,9 +1,177 @@
 #include <gtest/gtest.h>
+#include <memory>
+#include <iostream>
 #include "llob/PriceLevel.h"
 
+namespace llob {
 
-TEST(AA, BB) {
-  int a = 1;
-  EXPECT_EQ(a, 1);
+class ClassicPriceLevelTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    level = std::make_unique<ClassicPriceLevel>(1.0);
+    order_storage.reserve(1024);
+    internal_id = 0;
+  }
+
+  Order* makeOrder(OrderId id) {
+    Order order{};
+    NewOrderRequest nor(0, Side::Buy, 100.0, 10);
+    nor.setOrderId(id);
+    order.setFrom(nor);
+    order_storage.push_back(order);
+    return &order_storage.back();
+  }
+
+  void addOrders(int n) {
+    for (int i = 0; i < n; ++i)
+      level -> add(makeOrder(internal_id+i));
+  }
+
+  std::unique_ptr<ClassicPriceLevel> level;
+  std::vector<Order> order_storage;
+  std::size_t internal_id;
+};
+
+TEST_F(ClassicPriceLevelTest, EmptyOnConsutrction) {
+  EXPECT_EQ(level->size(), 0);
+  EXPECT_TRUE(level->empty());
 }
 
+TEST_F(ClassicPriceLevelTest, AddOrders) {
+  addOrders(3);
+  EXPECT_EQ(level->size(), 3);
+  EXPECT_FALSE(level->empty());
+  EXPECT_EQ(level->front()->id, 0);
+}
+
+TEST_F(ClassicPriceLevelTest, EraseFrontOrders) {
+  addOrders(3);
+  level->erase(&order_storage[0]);
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_EQ(level->front()->id, 1);
+}
+
+TEST_F(ClassicPriceLevelTest, EraseMiddleOrders) {
+  addOrders(3);
+  level->erase(&order_storage[1]);
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_EQ(level->front()->id, 0);
+}
+
+TEST_F(ClassicPriceLevelTest, EraseLastOrders) {
+  addOrders(3);
+  level->erase(&order_storage[2]);
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_EQ(level->front()->id, 0);
+}
+
+TEST_F(ClassicPriceLevelTest, EraseAllOrders) {
+  addOrders(3);
+  level->erase(&order_storage[0]);
+  level->erase(&order_storage[1]);
+  level->erase(&order_storage[2]);
+  EXPECT_EQ(level->size(), 0);
+  EXPECT_TRUE(level->empty());
+  EXPECT_EQ(level->front(), nullptr);
+}
+
+TEST_F(ClassicPriceLevelTest, AddAfterEraseAppendsToBack) {
+  addOrders(2);
+  level->erase(&order_storage[0]);
+  level->add(makeOrder(2));
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_FALSE(level->empty());
+  EXPECT_EQ(level->front()->id, 1);
+  level->erase(&order_storage[1]);
+  EXPECT_EQ(level->size(), 1);
+  EXPECT_FALSE(level->empty());
+  EXPECT_EQ(level->front()->id, 2);
+}
+
+class NodeBasedPriceLevelTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    level = std::make_unique<NodeBasedPriceLevel>(1.0);
+    order_storage.reserve(1024);
+    internal_id = 0;
+  }
+
+  OrderNode* makeOrderNode(OrderId id) {
+    Order order{};
+    NewOrderRequest nor(0, Side::Buy, 100.0, 10);
+    nor.setOrderId(id);
+    order.setFrom(nor);
+    OrderNode o_node;
+    o_node.order = std::move(order);
+    order_storage.push_back(o_node);
+    return &order_storage.back();
+  }
+
+  void addOrders(int n) {
+    for (int i = 0; i < n; ++i)
+      level -> add(makeOrderNode(internal_id+i));
+  }
+
+  std::unique_ptr<NodeBasedPriceLevel> level;
+  std::vector<OrderNode> order_storage;
+  std::size_t internal_id;
+};
+
+TEST_F(NodeBasedPriceLevelTest, EmptyOnConsutrction) {
+  EXPECT_EQ(level->size(), 0);
+  EXPECT_TRUE(level->empty());
+}
+
+TEST_F(NodeBasedPriceLevelTest, AddOrders) {
+  addOrders(3);
+  EXPECT_EQ(level->size(), 3);
+  EXPECT_FALSE(level->empty());
+  EXPECT_EQ(level->front()->order.id, 0);
+}
+
+TEST_F(NodeBasedPriceLevelTest, EraseFrontOrders) {
+  addOrders(3);
+  level->erase(&order_storage[0]);
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_EQ(level->front()->order.id, 1);
+}
+
+TEST_F(NodeBasedPriceLevelTest, EraseMiddleOrders) {
+  addOrders(3);
+  level->erase(&order_storage[1]);
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_EQ(level->front()->order.id, 0);
+}
+
+TEST_F(NodeBasedPriceLevelTest, EraseLastOrders) {
+  addOrders(3);
+  level->erase(&order_storage[2]);
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_EQ(level->front()->order.id, 0);
+}
+
+TEST_F(NodeBasedPriceLevelTest, EraseAllOrders) {
+  addOrders(3);
+  level->erase(&order_storage[0]);
+  level->erase(&order_storage[1]);
+  level->erase(&order_storage[2]);
+  EXPECT_EQ(level->size(), 0);
+  EXPECT_TRUE(level->empty());
+  EXPECT_EQ(level->front(), nullptr);
+}
+
+TEST_F(NodeBasedPriceLevelTest, AddAfterEraseAppendsToBack) {
+  addOrders(2);
+  level->erase(&order_storage[0]);
+  level->add(makeOrderNode(2));
+  EXPECT_EQ(level->size(), 2);
+  EXPECT_FALSE(level->empty());
+  EXPECT_EQ(level->front()->order.id, 1);
+  level->erase(&order_storage[1]);
+  EXPECT_EQ(level->size(), 1);
+  EXPECT_FALSE(level->empty());
+  EXPECT_EQ(level->front()->order.id, 2);
+}
+
+
+} // namespace llob
