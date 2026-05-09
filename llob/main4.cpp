7877ce5896625d802b907a604ca3b436d2a28e50
woonjeng <woonjeng@gmail.com>
2026-05-09 18:04:04 +0800

    adding secondary spsc queue for testing

--- llob/main4.cpp
+++ llob/main4.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <chrono>
-#include "llob/SPSCQueue2.h"
+#include "llob/SPSCQueue.h"
 
 struct A {
   const int b;
@@ -15,28 +15,26 @@ struct A {
 
 std::atomic<bool> running = false;
 
-void worker(llob::SPSCQueue2<A, 16>& q) {
+void worker(llob::SPSCQueue<A, 16>& q) {
   while (running) {
-    A* popped = q.front();
+    std::optional<A> popped = q.pop();
 
-    if (popped) {
-      std::cout << popped->toString() << "\n";
-      q.pop();
-    }
+    if (popped.has_value())
+      std::cout << (*popped).toString() << "\n";
   }
 }
 
 int main() {
   std::cout << "Start main\n";
   running.store(true);
-  llob::SPSCQueue2<A, 16> q;
+  llob::SPSCQueue<A, 16> q;
   std::thread t([&q](){ 
       worker(q);
   });
 
   for (int i = 0; i < 100 ; ++i) {
     A a{i};
-    while (!q.push(a)) 
+    while (!q.push(a))
       continue;
   }
 
